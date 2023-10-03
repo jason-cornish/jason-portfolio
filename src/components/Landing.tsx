7d3b063fb36260ba85f4c9edbb25e779274532ba
Jason Cornish (V) <jcornish@apple.com>
2023-10-03 09:37:37 -0500

    removed unused destination prop from linkbutton

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,64 +1,98 @@
 import React from "react";
+import { forwardRef } from "react";
 import styled from "styled-components";
+import AnimatedBlob from "./reusable/animated-blob";
 import LinkButton from "./reusable/LinkButton";
 import {
   HorizontalWrapper,
   VerticalWrapper,
 } from "./reusable/styled-components";
 
-const Landing = (props: any) => {
+const Landing = forwardRef((props: any, homeRef: any) => {
   return (
     <LandingWrapper>
-      <AnimatedText>
-        Hi, I'm Jason.
-        <br /> Front-End Web Developer.
-      </AnimatedText>
-      <Wrapper>
-        <p className="SubHeaderWide">
-          I like to design and develop intuitive, elegant online experiences.
-          <br /> I specialize in coding web applications with React, Typescript,
-          and StyledComponents.
-        </p>
-        <p className="SubHeaderMobile">
-          I like to design and develop intuitive, elegant online experiences. I
-          specialize in coding web applications with React, Typescript, and
-          StyledComponents.
-        </p>
-        <ButtonRow>
-          <LinkButton content={"About"} handleClick={props.handleLinkClick} />
-          <LinkButton content={"Work"} handleClick={props.handleLinkClick} />
-          <LinkButton
-            content={"Contact Me"}
-            handleClick={props.handleLinkClick}
-          />
-        </ButtonRow>
-      </Wrapper>
+      <LeftColumn>
+        <AnimatedText ref={homeRef}>
+          Hi, I'm Jason.
+          <br /> Front-End Web Developer.
+        </AnimatedText>
+        <Wrapper>
+          <p className="SubHeaderWide">
+            I like to design and develop intuitive, elegant online experiences.
+            <br /> I specialize in coding web applications with React,
+            Typescript, and StyledComponents.
+          </p>
+          <p className="SubHeaderMobile">
+            I like to design and develop intuitive, elegant online experiences.
+            I specialize in coding web applications with React, Typescript, and
+            StyledComponents.
+          </p>
+          <ButtonRow>
+            <LinkButton content={"About"} handleClick={props.handleLinkClick} />
+            <LinkButton content={"Work"} handleClick={props.handleLinkClick} />
+            <LinkButton
+              content={"Contact Me"}
+              handleClick={props.handleLinkClick}
+            />
+          </ButtonRow>
+        </Wrapper>
+      </LeftColumn>
+      <RightColumn>
+        <AnimationLayer>
+          <AnimatedBlob />
+        </AnimationLayer>
+      </RightColumn>
     </LandingWrapper>
   );
-};
+});
 
 export default Landing;
 
-const LandingWrapper = styled(VerticalWrapper)`
+const LandingWrapper = styled(HorizontalWrapper)`
   width: 85%;
-  padding-top: 250px;
-  padding-bottom: 250px;
   box-sizing: border-box;
-  column-gap: 40px;
+`;
+
+const LeftColumn = styled(VerticalWrapper)`
+  margin-right: 150px;
+  padding-top: 200px;
+  padding-bottom: 200px;
+
   @media screen and (max-width: 1200px) {
+    padding-top: 150px;
+    padding-bottom: 150px;
     width: 80%;
+    margin-right: 0px;
   }
   @media screen and (max-width: 700px) {
     align-items: center;
     padding-top: 150px;
-    padding-bottom: 200px;
+    padding-bottom: 150px;
     width: 90%;
+    margin-right: 0px;
   }
+`;
+
+const RightColumn = styled(VerticalWrapper)`
+  height: 100%;
+  position: relative;
+  padding-top: 200px;
+  padding-bottom: 200px;
+  @media screen and (max-width: 1200px) {
+    display: none;
+  }
+`;
+
+const AnimationLayer = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: flex-end;
 `;
 
 const AnimatedText = styled.h1`
   display: inline-block;
-  background: linear-gradient(
+  background-color: #e0e3e7;
+  /* background: linear-gradient(
     -45deg,
     #ee7752,
     #e73c7e,
@@ -67,13 +101,14 @@ const AnimatedText = styled.h1`
     #4831d4
   );
   background-size: 400% 400%;
-  animation: gradient 25s ease infinite;
+  animation: gradient 25s ease infinite; */
   position: relative;
   background-clip: text;
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
   font-size: 70px;
   margin: 0px;
+  font-weight: 600;
 
   @keyframes gradient {
     0% {
---
+++ src/components/reusable/animated-blob.tsx
@@ -0,0 +1,132 @@
+import styled from "styled-components";
+
+const AnimatedBlob = () => {
+  return (
+    <Wrapper>
+      <Blob>
+        <svg viewBox="0 0 248 248" xmlns="http://www.w3.org/2000/svg">
+          <filter id="noiseFilter">
+            <feTurbulence
+              type="turbulence"
+              baseFrequency="10.02"
+              numOctaves="1"
+              stitchTiles="stitch"
+            />
+          </filter>
+
+          <rect width="100%" height="100%" filter="url(#noiseFilter)" />
+        </svg>
+      </Blob>
+    </Wrapper>
+  );
+};
+
+export default AnimatedBlob;
+
+const Wrapper = styled.div`
+  position: relative;
+  padding-top: 20px;
+  animation: opacify 10s ease-in-out infinite;
+  @keyframes opacify {
+    0% {
+      opacity: 1;
+    }
+    33% {
+      opacity: 0.8;
+    }
+    66% {
+      opacity: 0.7;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+`;
+
+const Blob = styled.div`
+  svg {
+    width: 350px;
+    height: 350px;
+    filter: contrast(150%) brightness(70%);
+    background: radial-gradient(
+        circle at -24% -26%,
+        rgba(72, 49, 212, 1),
+        rgba(0, 0, 0, 0)
+      ),
+      radial-gradient(
+        circle at 66% 105%,
+        rgba(231, 60, 126, 1),
+        rgba(255, 0, 255, 0)
+      ),
+      radial-gradient(
+        circle at 71% 25%,
+        rgba(224, 227, 231, 1),
+        rgba(0, 255, 255, 0)
+      ),
+      radial-gradient(
+        circle at 17% 137%,
+        rgba(35, 166, 213, 1),
+        rgba(0, 0, 255, 0)
+      ),
+      url(/👆/that/noise.svg);
+
+    transition: all 10s ease-in-out;
+    animation: animate 10s ease-in-out infinite;
+
+    @media screen and (min-width: 1201px) {
+      @keyframes animate {
+        0% {
+          filter: contrast(150%) brightness(70%);
+          border-radius: 43% 57% 41% 59% / 58% 32% 68% 42%;
+          background-position: 0% 25%;
+        }
+        33% {
+          filter: contrast(170%) brightness(70%);
+          border-radius: 74% 26% 51% 49% / 72% 48% 52% 28%;
+          background-position: 50% 50%;
+        }
+        66% {
+          filter: contrast(160%) brightness(70%);
+          border-radius: 38% 62% 60% 40% / 72% 40% 60% 28%;
+          background-position: 100% 50%;
+        }
+        100% {
+          filter: contrast(150%) brightness(70%);
+          border-radius: 43% 57% 41% 59% / 58% 32% 68% 42%;
+          background-position: 0% 25%;
+        }
+      }
+    }
+
+    @media screen and (max-width: 1200px) {
+      display: none;
+      padding-top: 60px;
+      @keyframes animate {
+        0% {
+          width: 300px;
+          height: 300px;
+          border-radius: 43% 57% 41% 59% / 58% 32% 68% 42%;
+          background-position: 0% 50%;
+        }
+        33% {
+          height: 300px;
+          width: 300px;
+          border-radius: 74% 26% 51% 49% / 72% 48% 52% 28%;
+          background-position: 50% 50%;
+        }
+        66% {
+          height: 300px;
+          width: 300px;
+          border-radius: 38% 62% 60% 40% / 72% 40% 60% 28%;
+          background-position: 100% 50%;
+        }
+        100% {
+          height: 300px;
+          width: 300px;
+          border-radius: 43% 57% 41% 59% / 58% 32% 68% 42%;
+          background-position: 0% 50%;
+        }
+      }
+    }
+  }
+`;
